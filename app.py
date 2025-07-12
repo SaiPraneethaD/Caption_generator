a3c668cde93ffbad6ea09ef60e202b3f82d2c24b
SaiPraneethaD <150116841+SaiPraneethaD@users.noreply.github.com>
2025-07-12 16:21:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-
+import streamlit as st
+from PIL import Image
+st.title("hello page")
+st.write("upload a image")
+upload_file=st.file_uploader("choose your file...",type=["jpg","png","jpeg"])
+if upload_file is not None:
+  st.success("successfully uploaded")
+  image=Image.open(upload_file)
+  st.image(image,caption="uploaded image"
